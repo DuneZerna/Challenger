c3a233355a714e72961a5e27b5068c45a5f28e6a
Dune Zerna <duzer17@student.sdu.dk>
2019-11-08 09:57:43 +0100

    Initial Commit

---
+++ Challenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenger
+//
+//  Created by Dune Zerna on 08/11/2019.
+//  Copyright © 2019 University of Southern Denmark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
