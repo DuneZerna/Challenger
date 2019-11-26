6cc474e067c46c4f8431862706dbd511660b8521
Dune Zerna <duzer17@student.sdu.dk>
2019-11-26 14:13:44 +0100

    History
    History view er implementeret.
    Challenge virker fortsat ikke.

---
+++ Challenger/Challenger/Utils/HistorieTableCell.swift
@@ -0,0 +1,17 @@
+//
+//  HistorieTableCell.swift
+//  progress
+//
+//  Created by Pernille Madsen on 19/11/2019.
+//  Copyright © 2019 Pernille Madsen. All rights reserved.
+//
+
+import UIKit
+
+// Dune: If this gives an error, try refactor rename this to HistorieTableCell
+class HistoryTableCell: UITableViewCell {
+
+    @IBOutlet weak var tableCell: UILabel!
+    
+
+}
